97c3ad4533deb1437ec2bf5ccc65c08ffbc3d4fe
Adilson Krasovski <kalangable@gmail.com>
2015-12-06 17:36:56 -0200

    Incluido processo de imagem - Não concluido

--- src/main/webapp/js/admin/controllers.js
+++ src/main/webapp/js/admin/controllers.js
@@ -64,3 +64,36 @@ app.controller('categoriasController', function($scope, categoriaService) {
 	basic_controller($scope, categoriaService, 'categorias')
 	
 })
+
+app.controller('imagensController', function($scope, $http, config) {
+	
+	$scope.listaImagem=[]
+	
+	var getImagem = function () {
+		$scope.view_atual = 'loading'
+		$http.get(config.baseUrlImagem).success(function (data) {
+			$scope.listaImagem = data
+			$scope.view_atual = 'listagem'
+	})};
+	
+	$scope.cadastroImagem = function(){
+		$scope.view_atual = 'cadastro'
+	};
+	
+	$scope.salvarImagem = function(element){
+		console.log(element)
+	};
+	
+	$scope.listagem = function(){
+		getImagem()
+	};
+	
+	var inserirImagem = function (imagem) {
+		$scope.view_atual = 'loading'
+		$http.get(config.baseUrlImagem).success(function (data) {
+			$scope.listaImagem = data
+			$scope.view_atual = 'listagem'
+	})};
+	
+	getImagem();
+})
