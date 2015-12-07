d311d2767445f443a21e49126c07ee83fccfa0fe
Adilson Krasovski <kalangable@gmail.com>
2015-12-07 12:48:56 -0200

    Incluir o ID para Pedido.

--- src/main/java/com/elono/Application.java
+++ src/main/java/com/elono/Application.java
@@ -4,17 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import com.elono.model.Pedido;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer  {
+public class Application extends SpringBootServletInitializer {
 
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
+	protected SpringApplicationBuilder configure( SpringApplicationBuilder application ) {
+		return application.sources( Application.class );
 	}
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+	public static void main( String[] args ) throws Exception {
+		ConfigurableApplicationContext ctx = SpringApplication.run( Application.class, args );
+
+		RepositoryRestConfiguration restConfiguration = ctx.getBean( RepositoryRestConfiguration.class );
+
+		restConfiguration.exposeIdsFor( Pedido.class );
 	}
-    
+
 }
